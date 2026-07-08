b804a3433c524458f9dddafc4a34f5dadf132af9
AgusSalim-Sihombing <agussalim210420@gmail.com>
2026-07-08 15:19:51 +0700

    update

--- mobile_app/lib/config/api_config.dart
+++ mobile_app/lib/config/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
-  static const String baseUrl = 'http://172.20.23.17:3000/api';
+  static const String baseUrl = 'http://192.168.1.12:3000/api';
   // static const String baseUrl = 'https://gaming-overdrive-freemason.ngrok-free.dev/api';
 }
